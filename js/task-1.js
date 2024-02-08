bddc19b02c8ebf357407efebead76e744173bdca
ArtemNozhenko <artem2516nozh@gmail.com>
2024-02-08 23:30:00 +0100

    add

--- js/task-1.js
+++ js/task-1.js
@@ -6,10 +6,238 @@
 // console.log(createReversedArray(12, 85, 37, 4));
 // console.log(createReversedArray(412, 371, 94, 63, 176));
 
-function calculateTax(amount, taxRate = 0.2) {
-  let sum = amount * taxRate;
-  return sum;
-}
+// function calculateTax(amount, taxRate = 0.2) {
+//   let sum = amount * taxRate;
+//   return sum;
+// }
 
-console.log(calculateTax(100));
-console.log(calculateTax(200, 0.2));
+// console.log(calculateTax(100));
+// console.log(calculateTax(200, 0.2));
+
+// 1
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = Object.keys(apartment);
+// const values = [];
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// 2
+// function countProps(object) {
+//   let propCount = Object.keys(object);
+
+//   return propCount.length;
+// }
+
+// console.log(countProps({}));
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   const salary = Object.values(salaries);
+//   for (const value of salary) {
+//     totalSalary += value;
+//   }
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (let color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//     return null;
+//   }
+// }
+// console.log(getProductPrice('Radar'));
+// console.log(getProductPrice('Engine'));
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const allKeys = [];
+//   for (const product of products) {
+//     if (propName in product) {
+//       allKeys.push(product[propName]);
+//     }
+//   }
+//   return allKeys;
+// }
+
+// console.log(getAllPropValues('quantity'));
+// console.log(getAllPropValues('category'));
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price * product.quantity;
+//     }
+//   }
+//   return `Product ${productName} not found!`;
+// }
+
+// console.log(calculateTotalPrice('Radar'));
+// console.log(calculateTotalPrice('Droid'));
+// console.log(calculateTotalPrice('Grip'));
+// console.log(calculateTotalPrice('category'));
+
+// const atTheOldToad = {
+//   potions: [],
+//   getPotions() {
+//     return `List of all available potions`;
+//   },
+//   addPotion(potionName) {
+//     return `Adding ${potionName}`;
+//   },
+// };
+
+// console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.addPotion('Invisibility'));
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   updatePotionName(oldName, newName) {
+//     for (const potion of this.potions) {
+//       if (potion.name === oldName) {
+//         return (potion.name = newName);
+//       }
+//     }
+//   },
+// };
+
+// atTheOldToad.updatePotionName('Stone skin', 'Invisibility');
+// atTheOldToad.updatePotionName('Speed potion', 'Polymorth');
+// console.log(atTheOldToad.potions);
+
+// function getExtremeScores(scores) {
+//   const extremeScores = {
+//     best: Math.max(...scores),
+//     worst: Math.min(...scores),
+//   };
+//   return extremeScores;
+// }
+
+// console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
+// console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+
+// 5 module ///
+
+// 1
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// console.log(makeMessage('Ultracheese', deliverPizza));
+
+// 2
+// function changeEven(numbers, value) {
+//   const newArr = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       newArr.push(numbers[i] + value);
+//     } else {
+//       newArr.push(numbers[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+
+// 3
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  {
+    title: 'The Dreams in the Witch House',
+    author: 'Howard Lovecraft',
+    rating: 8.67,
+  },
+];
+
+const MIN_BOOK_RATING = 8;
+
+const names = books
+  .filter(book => book.rating > MIN_BOOK_RATING)
+  .map(book => book.author)
+  .toSorted((a, b) => a.localeCompare(b));
+
+console.log(names);
+// console.log(sortedByReversedAuthorName);
+// console.log(sortedByAscendingRating);
+// console.log(sortedByDescentingRating);
